c75d87db2a3436eb2bf2cfa685b6d0c82cfc1d47
DaVinCi <fox.winner9@gmail.com>
2023-07-31 09:45:02 +0300

    add component scan for spring boot application

--- src/main/java/com/example/hm_neto_jdbc/application/HmNetoJdbcApplication.java
+++ src/main/java/com/example/hm_neto_jdbc/application/HmNetoJdbcApplication.java
@@ -2,8 +2,10 @@ package com.example.hm_neto_jdbc.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.example.hm_neto_jdbc")
 public class HmNetoJdbcApplication {
 
     public static void main(String[] args) {
